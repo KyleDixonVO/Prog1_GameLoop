551fc28b42eb3a688b71617b19b81e910c5956e6
KyleDixonVO <KyleDixon@hotmail.ca>
2021-10-19 09:46:47 -0300

    Bounds Check, Erasing Previous Characters, Setting Screen and Buffer Size

--- Prog1_GameLoop/Program.cs
+++ Prog1_GameLoop/Program.cs
@@ -9,35 +9,47 @@ namespace Prog1_GameLoop
     class Program
     {
         static bool gameOver;
-        static int x = 50;
-        static int y = 50;
+        static int x = 20;
+        static int y = 20;
+        static int previous_cursor_x;
+        static int previous_cursor_y;
 
         static void Main(string[] args)
         {
+            Console.CursorVisible=false;
+            Console.SetWindowSize(40, 40);
+            Console.SetBufferSize(80, 80);
+            
             while (gameOver == false)
             {
                 PlayerUpdate();
+                BoundsCheck();
                 PlayerDraw(x, y);
             }
 
         }
         
         static void PlayerDraw(int x, int y)
-        {
-            Console.Write("x");
+        {   
+            
+            Console.Clear();
             Console.SetCursorPosition(x, y);
             Console.Write("X");
         }
 
         static void PlayerUpdate()
         {
-            //char input;
+            //*CHARS NEED TO HAVE SINGLE QUOTES TO BE REFERENCED*
+
             //ConsoleKeyInfo readKeyInput;
             //readKeyInput = Console.ReadKey(true);
-            //input = readKeyInput.KeyChar;
-            
-            
-                Console.ReadKey(true);
+            //char input = readKeyInput.KeyChar;
+            //if (input == 'd')
+            //  {
+            //      Do Stuff
+            //  }
+
+            Console.ReadKey(true);
 
                 if (Console.ReadKey(true).Key == ConsoleKey.W)
                 {
@@ -57,5 +69,29 @@ namespace Prog1_GameLoop
                 }
             
         }
+
+        static void BoundsCheck()
+        {
+            if(x<0)
+            {
+                x = 0;
+            }
+            
+            if (x>40)
+            {
+                x = 40;
+            }
+
+            if(y<0)
+            {
+                y = 0;
+            }
+            
+            if(y>40)
+            {
+                y = 40;
+            }
+        }
+
     }
 }
